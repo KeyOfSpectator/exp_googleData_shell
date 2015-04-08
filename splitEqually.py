c1cac468211084d0351cf3b1cbabadd7684932c0
ubuntu <ubuntu@m1.(none)>
2015-04-08 21:25:04 +0800

    update for readme

--- splitEqually.py
+++ splitEqually.py
@@ -5,12 +5,12 @@ import os
 import csv
 import time
 
-NUM = 32
+NUM = 16
 
 #INPUT_PATH = "/root/expdata/media/EEAEDA2FAED9EFD7/GoogleCluster/UsedData/fewer_throughput/split1/"
-INPUT_PATH = "/home/ubuntu/data/output_tsdb/input/"
+INPUT_PATH = "/home/ubuntu/data/"
 #OUTPUT_PATH = "/root/expdata/media/EEAEDA2FAED9EFD7/GoogleCluster/UsedData/fewer_throughput/split" + str(NUM) + "/"
-OUTPUT_PATH = "/home/ubuntu/data/output_tsdb/output" + str(NUM) + "/"
+OUTPUT_PATH = "/home/ubuntu/data/output" + str(NUM) + "/"
 
 INPUT_FILE_LIST = sorted([name for name in os.listdir(INPUT_PATH) if name.endswith('.csv')])
 OUTPUT_FILE_POOL = [open(OUTPUT_PATH + "" + str(i) + ".csv", 'w') for i in range(NUM)]
